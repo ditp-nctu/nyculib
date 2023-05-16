41bc94d453465f0cb389acd070a941d64759844c
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2023-05-16 14:03:51 +0800

    init commit

---
+++ src/main/java/art/cctcc/nycu/lib/NYCULibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package art.cctcc.nycu.lib;
+
+/**
+ *
+ * @author Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
+ */
+public class NYCULibrary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
