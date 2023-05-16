3dd0f14306f1d65b05b9f124b409135dae05dbca
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2023-05-17 07:41:00 +0800

    add simple query

--- src/main/java/art/cctcc/nycu/lib/NYCULibrary.java
+++ src/main/java/art/cctcc/nycu/lib/NYCULibrary.java
@@ -1,8 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package art.cctcc.nycu.lib;
+
+import static art.cctcc.nycu.lib.Settings.NS;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import org.apache.jena.query.QuerySolution;
+import tech.metacontext.utils.SparqlQuery;
 
 /**
  *
@@ -10,7 +15,28 @@ package art.cctcc.nycu.lib;
  */
 public class NYCULibrary {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  private static String queryString
+          = """
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            
+            PREFIX : <%s>
+            
+            SELECT ?s ?p
+            WHERE {
+              ?s a rdfs:Class; #owl:Class ;
+                ?p []
+            }
+            """;
+  private static Consumer<QuerySolution> processor = t -> {
+    System.out.println(t);
+  };
+
+  public static void main(String[] args) {
+
+    var source_folder = Path.of("nyculib");
+    var sq = new SparqlQuery(source_folder,
+            String.format(queryString, NS));
+    sq.processResults(processor);
+  }
 }
---
+++ src/main/java/art/cctcc/nycu/lib/Settings.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package art.cctcc.nycu.lib;
+
+/**
+ *
+ * @author Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
+ */
+public class Settings {
+
+  public static final String NS = "http://lib.nycu.edu.tw/ontology#";
+}
