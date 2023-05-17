ba9d2c22ac0577a6695ed77e12549db1d2a99493
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2023-05-17 10:18:05 +0800

    minor adjustment.

--- src/main/java/art/cctcc/nycu/lib/NYCULibrary.java
+++ src/main/java/art/cctcc/nycu/lib/NYCULibrary.java
@@ -5,8 +5,12 @@ package art.cctcc.nycu.lib;
 
 import static art.cctcc.nycu.lib.Settings.NS;
 import java.nio.file.Path;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Consumer;
 import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import tech.metacontext.utils.SparqlQuery;
 
 /**
@@ -15,6 +19,8 @@ import tech.metacontext.utils.SparqlQuery;
  */
 public class NYCULibrary {
 
+  private static final Map<String, String> ONT_PREFIX_MAP
+          = ModelFactory.createOntologyModel().getNsPrefixMap();
   private static String queryString
           = """
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -24,19 +30,29 @@ public class NYCULibrary {
             
             SELECT ?s ?p
             WHERE {
-              ?s a rdfs:Class; #owl:Class ;
+              ?s a rdfs:Class;
                 ?p []
             }
             """;
+
   private static Consumer<QuerySolution> processor = t -> {
-    System.out.println(t);
+    var ite = t.varNames();
+    while (ite.hasNext()) {
+      var name = ite.next();
+      var res = t.getResource(name).toString();
+      for (Entry<String, String> entry : ONT_PREFIX_MAP.entrySet()) {
+        res = res.replace(entry.getValue(), entry.getKey() + ":");
+      }
+      System.out.print(res + "\t");
+    }
+    System.out.println();
   };
 
   public static void main(String[] args) {
 
     var source_folder = Path.of("nyculib");
-    var sq = new SparqlQuery(source_folder,
-            String.format(queryString, NS));
+    var sq = new SparqlQuery(source_folder, String.format(queryString, NS));
+    ONT_PREFIX_MAP.put("", NS);
     sq.processResults(processor);
   }
 }
